6364bbb6e6071279ee4ef0df4838f67f4c24095d
Yeison-pacheco08 <ypachecorangel15@correo.unicordoba.edu.co>
2021-05-05 10:34:19 -0500

    Creación API básica
    Se realiza un mensaje de Bienvenida

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const express = require('express'); 
+const app = express();
+const path = require('path');
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join (__dirname,  'views/index.html'));
+});
+
+
+app.listen(3000, function () {
+  console.log('Escuchando en el puerto 3000');
+
+});
